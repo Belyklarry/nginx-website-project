bb82d91d7412583d5d4458fd19309fd217061afe
larry <larrymoyo.devops@gmail.com>
2022-05-19 17:57:08 +0000

    added terraform variables

--- terraform/static-site/variables.tf
+++ terraform/static-site/variables.tf
@@ -1,11 +1,17 @@
 variable "region" {
     description= "The region our instance will be in (i.e. us-east-1)"
+    type= string
+    default= "af-south-1"
 }
 
 variable "name" {
     description= "The name of the instance we are creating"
+    type= string
+    default= "hello"
 }
 
 variable "group" {
     description= "the name of the group we will be using for Ansible purposes"
+    type= string
+    default= "web"
 }
